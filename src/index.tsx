fd7c05a79940fbc3af228349a43f484de615de98
Bruno Sobral <brunosobralss@hotmail.com>
2022-09-05 17:30:18 -0300

    feat: miragejs-get-route

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 
 import { App } from './App';
+
+// http://localhost:3000/api/...rotas
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    // quando houver uma requisição 'GET'
+    this.get('/transactions', () => {
+      return [
+        {
+          id: '',
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          created_at: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
