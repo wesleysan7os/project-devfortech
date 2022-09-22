6b17a9b2f83966299f298878318db9e5481fc476
brunosobralss <brunosobralss@hotmail.com>
2022-09-22 01:16:25 -0300

    fix: cursor style at input

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -24,7 +24,7 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button{
+  body, textarea, button{
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
 
